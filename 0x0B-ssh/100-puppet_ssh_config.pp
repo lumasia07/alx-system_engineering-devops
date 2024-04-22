00a23397656cdf0237148c214559d7edfee8fd63
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-22 05:08:23 -0700

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentifyFile ~/.ssh/school',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
