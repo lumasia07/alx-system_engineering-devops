dbfb9277ecd4d241c92c6544eb6dfd8821f386d5
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-22 05:01:14 -0700

    config puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Creates an ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentifyFile ~/.ssh/school',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentifyFile ~/.ssh/school',
+}
