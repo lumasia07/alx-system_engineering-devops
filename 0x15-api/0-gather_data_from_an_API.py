d085af7c1bcc4acc52dd05192d7042835367cda9
lumasia07 <lumasiastancelaus@gmail.com>
2024-05-20 05:46:00 -0700

    gather

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    completed = [x.get("title") for x in todos if x.get("completed") is True]
+    print("Employee {} is done with ({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(fin)) for fin in completed]
