b71eca45598e18fbe26528b8c385453b1f8fdf87
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-06-04 16:43:14 +0300

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Queries titles of the top 10 reddit posts"""
 
-import requests
+from requests import get
 
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Google Chrome Version 124.0.6367.78'}
+    headers = {'User-agent': 'Google Chrome Version 124.0.6367.78'}
 
     try:
-        response = requests.get(url, headers=headers)
+        response = get(url, headers=headers)
         top_data = response.json()
         hot_data = top_data.get('data').get('children')
 
-        if 'error' in data:
+        if 'error' in top_data:
             print(None)
             return
 
