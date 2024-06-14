bc7397e4f1f0ae53b8b0aac9eb45f54b8f6cd138
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-06-14 09:26:32 +0300

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,20 @@
-# Increases traffic requests on Nginx
+exec { 'increase_worker_connections':
+  command => 'sed -i "s/worker_connections .*/worker_connections 1000;/" /etc/nginx/nginx.conf',
+  path    => ['/usr/bin', '/bin', '/usr/local/bin'],
+  unless  => 'grep -q "worker_connections 1000;" /etc/nginx/nginx.conf',
+}
 
-exec { 'increase_ulimit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/bin/', '/bin/', '/usr/local/bin/'],
-  unless  => 'grep -q "4096" /etc/default/nginx',
+exec { 'increase_worker_rlimit_nofile':
+  command => 'sed -i "s/worker_rlimit_nofile .*/worker_rlimit_nofile 4096;/" /etc/nginx/nginx.conf',
+  path    => ['/usr/bin', '/bin', '/usr/local/bin'],
+  unless  => 'grep -q "worker_rlimit_nofile 4096;" /etc/nginx/nginx.conf',
 }
 
 exec { 'nginx-restart':
   command     => '/etc/init.d/nginx restart',
-  path        => ['/etc/init.d/', '/usr/bin/'],
-  subscribe   => Exec['increase_ulimit'],
+  path        => ['/etc/init.d', '/usr/bin'],
+  subscribe   => Exec['increase_worker_connections', 'increase_worker_rlimit_nofile'],
   refreshonly => true,
 }
 
-Exec['increase_ulimit'] -> Exec['nginx-restart']
+Exec['increase_worker_connections'] ~> Exec['increase_worker_rlimit_nofile'] ~> Exec['nginx-restart']
