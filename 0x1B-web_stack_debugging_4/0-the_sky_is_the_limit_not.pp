2c05320d9568606e7720485a7022a547ecc0421c
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-06-14 09:45:33 +0300

    fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,13 +2,13 @@
 
 exec { 'increase_ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/bin/', '/bin/', '/usr/local/bin/'],
+  path    => ['/usr/bin', '/bin', '/usr/local/bin'],
   unless  => 'grep -q "4096" /etc/default/nginx',
 }
 
 exec { 'nginx-restart':
   command     => '/etc/init.d/nginx restart',
-  path        => ['/etc/init.d/', '/usr/bin/'],
+  path        => ['/etc/init.d', '/usr/bin', '/bin'],
   subscribe   => Exec['increase_ulimit'],
   refreshonly => true,
 }
